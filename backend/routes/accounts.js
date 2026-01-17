32a4fe3f4b391035f38a3ad72e02f8367be8a2c8
Laura <laurasimila95@gmail.com>
2026-01-17 12:42:36 +0200

    CRUD/accounts testailua

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,6 +1,11 @@
+/* Tämä tiedosto on Express-reititin accounts -resurssille. Se liittää HTTP-pyynnöt -> accountsControllerin funktioihin ja toteuttaa accounts-
+taulun CRUD-reitit */
+
+// Express Router
 const express = require('express');
 const router = express.Router();
 
+// Controller-funktioiden tuonti
 const {
     getAccountById,
     createAccount,
@@ -8,9 +13,11 @@ const {
     deleteAccount,
 } = require('../controllers/accountsController');
 
-router.get('/:idAccount', getAccountById);
-router.post('/', createAccount);
-router.put('/:idAccount', updateAccount);
-router.delete('/:idAccount', deleteAccount);
+// Reitit = CRUD
+router.get('/:idAccount', getAccountById); // READ - hae tili
+router.post('/', createAccount); // CREATE - luo tili
+router.put('/:idAccount', updateAccount); // UPDATE - päivitä tili
+router.delete('/:idAccount', deleteAccount); // DELETE - poista tili
 
+// Export
 module.exports = router;
